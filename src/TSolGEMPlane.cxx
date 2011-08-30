a3a86e1ca2a715031d13825fbb706fc648a3081d
Richard S. Holmes <rsholmes@physics.syr.edu>
2011-08-30 17:32:42 +0000

    Member data and Get functions for strip geometry

--- src/TSolGEMPlane.cxx
+++ src/TSolGEMPlane.cxx
@@ -3,7 +3,12 @@
 #include "TSolGEMCluster.h"
 #include "THaEvData.h"
 
-TSolGEMPlane::TSolGEMPlane( const char *name, const char *desc ){
+TSolGEMPlane::TSolGEMPlane( const char *name, const char *desc )
+  : fNStrips (100), // some dummy values for now, will be filled from database
+    fSBeg (-1.0),
+    fSPitch (0.0001),
+    fSAngle (0.0)
+{
     printf("I'm a GEM plane named %s\n", name );
 
     fClusters = new TClonesArray("TSolGEMCluster", 100);
--- src/TSolGEMPlane.h
+++ src/TSolGEMPlane.h
@@ -20,11 +20,28 @@ class TSolGEMPlane : public THaDetector {
 	GEMDir_t GetDirection(){ return fDir; }
 	TSolGEMPlane *GetPairedPlane() { return fPairPlane; }
 
+	Int_t    GetNStrips()  const { return fNStrips; }
+
+	// Following assumes parallel strips, later we may
+	// need to generalize this
+
+	Double_t GetSBeg()     const { return fSBeg; }
+	Double_t GetSPitch()   const { return fSPitch; }
+	Double_t GetSAngle()   const { return fSAngle; }
+	Double_t GetSAngleComp() const { return 3.14159/2 - fSAngle; }
 
     private:
 	TClonesArray  *fClusters; // Clusters
 	GEMDir_t fDir;		 // Plane orientation
 	TSolGEMPlane *fPairPlane; // Paired plane
+
+	Int_t fNStrips;     // Number of strips
+	Double_t fSBeg;     // Position of 1st strip (low edge) (m)
+                            // i.e. distance to detector origin normal to strip
+	Double_t fSPitch;   // Strip pitch (m)
+	Double_t fSAngle;   // Angle (rad) between horizontal axis
+                            // and normal to strips in dir of increasing
+                            // strip position
 
     public:
 	ClassDef(TSolGEMPlane,1)
