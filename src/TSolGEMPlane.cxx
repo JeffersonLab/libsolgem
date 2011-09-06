96242457b873690e4d33d1aff5348d0661861f94
Richard S. Holmes <rsholmes@physics.syr.edu>
2011-09-06 17:57:25 +0000

    Make plane a sub detector of a chamber

--- src/TSolGEMPlane.cxx
+++ src/TSolGEMPlane.cxx
@@ -1,13 +1,19 @@
-#include "TClonesArray.h"
 #include "TSolGEMPlane.h"
+
+#include <iostream>
+
+#include "TClonesArray.h"
 #include "TSolGEMCluster.h"
 #include "THaEvData.h"
 
+using namespace std;
+
 TSolGEMPlane::TSolGEMPlane( const char *name, const char *desc )
-  : fNStrips (100), // some dummy values for now, will be filled from database
+  : fDir (kGEMX),
+    fNStrips (100), // some dummy values for now, will be filled from database
     fSBeg (-1.0),
     fSPitch (0.0001),
-    fSAngle (0.0)
+    fPixelFactor (2)
 {
     printf("I'm a GEM plane named %s\n", name );
 
@@ -25,3 +31,19 @@ Int_t TSolGEMPlane::Decode( const THaEvData &d ){
 
     return 0;
 }
+
+Double_t 
+TSolGEMPlane::GetSAngle()   const
+{
+  if (fDir == kGEMX)
+    return 3.14159/2;
+  else if (fDir == kGEMY)
+    return 0.0;
+  else
+    {
+      cerr << __FUNCTION__ << " Strip angle undefined" << endl;
+      return 9999.0;
+    }
+}
+
+
--- src/TSolGEMPlane.h
+++ src/TSolGEMPlane.h
@@ -1,15 +1,14 @@
 #ifndef __TSOLGEMPLANE_H
 #define __TSOLGEMPLANE_H
 
-#include "THaDetector.h"
-#include "THaEvData.h"
+#include "THaSubDetector.h"
 
 #include "types.h"
 
 class TSolGEMCluster;
 class TClonesArray;
 
-class TSolGEMPlane : public THaDetector {
+class TSolGEMPlane : public THaSubDetector {
     public:
 	TSolGEMPlane(const char *name, const char *desc);
 	virtual ~TSolGEMPlane() {;}
@@ -21,14 +20,19 @@ class TSolGEMPlane : public THaDetector {
 	TSolGEMPlane *GetPairedPlane() { return fPairPlane; }
 
 	Int_t    GetNStrips()  const { return fNStrips; }
+	Double_t GetPPitch()   const { return fSPitch/fPixelFactor;} // Pixel pitch
+	Int_t    GetNPixels()  const { return fNStrips * fPixelFactor; }
 
 	// Following assumes parallel strips, later we may
 	// need to generalize this
 
 	Double_t GetSBeg()     const { return fSBeg; }
 	Double_t GetSPitch()   const { return fSPitch; }
-	Double_t GetSAngle()   const { return fSAngle; }
-	Double_t GetSAngleComp() const { return 3.14159/2 - fSAngle; }
+	Double_t GetSAngle()   const; // Angle (rad) between horizontal axis
+                                      // and normal to strips in dir of
+	                              // increasing strip position
+	Double_t GetSAngleComp() const { return 3.14159/2 - GetSAngle(); }
+	Double_t GetPixelLocation (UInt_t i) const { return GetSBeg() + i * GetPPitch();} // Lower left corner of pixel i
 
     private:
 	TClonesArray  *fClusters; // Clusters
@@ -39,9 +43,7 @@ class TSolGEMPlane : public THaDetector {
 	Double_t fSBeg;     // Position of 1st strip (low edge) (m)
                             // i.e. distance to detector origin normal to strip
 	Double_t fSPitch;   // Strip pitch (m)
-	Double_t fSAngle;   // Angle (rad) between horizontal axis
-                            // and normal to strips in dir of increasing
-                            // strip position
+	Int_t fPixelFactor;  // Pixels per strip
 
     public:
 	ClassDef(TSolGEMPlane,1)
