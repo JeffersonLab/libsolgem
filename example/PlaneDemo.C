dfc3f7a49854d9bff1d2593185ea3cd4bb739f40
Richard S. Holmes <rsholmes@physics.syr.edu>
2011-09-26 17:23:35 +0000

    Chamber now inits its own planes

--- example/PlaneDemo.C
+++ example/PlaneDemo.C
@@ -3,12 +3,7 @@ void PlaneDemo()
   gROOT->LoadMacro("libsolgem.so");
   TSolGEMChamber y("testchamber","Test chamber");
   y.SetName("testchamber");
-  y.InitPlane(0,"testplanex","Test plane X");
-  y.GetPlane(0).SetName("testplanex");
-  y.GetPlane(0).Init();
-  y.InitPlane(1,"testplaney","Test plane Y");
-  y.GetPlane(1).SetName("testplaney");
-  y.GetPlane(1).Init();
+  y.Init();
 
   y.GetPlane(0).Print();
   y.GetPlane(1).Print();
