2ae883f805afdda355f341efddceb429bfa9a0cc
Ole Hansen <ole@jlab.org>
2013-08-30 20:04:46 +0000

    Bundle all those parallel C-arrays into a neat vector<GEMHitData_t>. Watch how pages of code collapse into single lines :)
    Remove unused fEntryNumber and add fSource to hit data. fSource
    indicates the MC source: 0 = signal, >0 some type of background
    (meaning to be defined by the user).

--- src/TSolGEMData.cxx
+++ src/TSolGEMData.cxx
@@ -6,19 +6,7 @@ using namespace std;
 
 TSolGEMData::TSolGEMData (UInt_t h)
 {
-    fGem  = 0;
-    fEdep = 0;
-    fTime = 0;
-    fPID  = 0;
-    fType = 0;
-    fXi   = 0;
-    fXo   = 0;
-    fXr   = 0;
-    fMom  = 0;
-    fVert  = 0;
-    fEntryNumber  = 0;
-
-  if (h > 0) InitEvent (h);
+  InitEvent (h);
 
   // Initialize variables
 }
@@ -28,37 +16,10 @@ TSolGEMData::~TSolGEMData()
   ClearEvent();
 }
 
-void 
+void
 TSolGEMData::ClearEvent() 
 {
-  if (fGem)
-  {
-    delete[] fGem;
-    delete[] fEdep;
-    delete[] fTime;
-    delete[] fPID;
-    delete[] fType;
-    delete[] fEntryNumber;
-    
-    for (UInt_t k = 0; k < fNHit; k++) 
-    {
-      delete fXi[k];
-      delete fXo[k];
-      delete fXr[k];
-      delete fMom[k];
-      delete fVert[k];
-    }
-    delete[] fXi;
-    delete[] fXo;
-    delete[] fXr;
-    delete[] fMom;
-    delete[] fVert;
-    
-    //    cerr << __FUNCTION__ << "fGEM deleted " << endl;
-    
-    fGem = 0;
-    fNHit = 0;    
-  }
+  fHitData.clear();
 };
 
 void
@@ -67,100 +28,19 @@ TSolGEMData::InitEvent (UInt_t h)
   if (h <= 0)
     return;
 
-  fNHit = h; // number of hits in event
-
-  // Hits
-
-  fGem = new UInt_t[h]; // chamber with hit
-  fEdep = new Double_t[h]; // energy lost in drift
-  fTime = new Double_t[h]; // Hit time
-  fPID = new UInt_t[h]; // particle ID
-  fType = new UInt_t[h]; // particle type
-  fEntryNumber = new UInt_t[h]; // entry in file
-  fXi = new TVector3*[h];
-  fXo = new TVector3*[h];
-  fXr = new TVector3*[h];
-  fMom = new TVector3*[h];
-  fVert = new TVector3*[h];
-  for (UInt_t k = 0; k < fNHit; k++) 
-  {
-  	fXi[k] = new TVector3;
-  	fXo[k] = new TVector3;
-  	fXr[k] = new TVector3;
-  	fMom[k] = new TVector3;
-  	fVert[k] = new TVector3;
-  }	  		
+  fHitData.reserve(10*h);
+  fHitData.resize(h);
 }
 
 void 
 TSolGEMData::AddGEMData(TSolGEMData *gd)
 {
-    // Create appropriately sized storage and copy
+  // Add hit data from 'gd' to the hit data of this object
 
-    UInt_t  Nnewhit = gd->GetNHit();
+  if( !gd || gd->GetNHit() == 0 )
+    return;
 
-    UInt_t *newfGem    = new UInt_t[fNHit + Nnewhit];
-    Double_t *newfEdep = new Double_t[fNHit + Nnewhit];
-    Double_t *newfTime = new Double_t[fNHit + Nnewhit];
-    UInt_t *newfPID    = new UInt_t[fNHit + Nnewhit];
-    UInt_t *newfType   = new UInt_t[fNHit + Nnewhit];
-    UInt_t *newfEntryNumber = new UInt_t[fNHit + Nnewhit];
-    TVector3 **newfXi = new TVector3*[fNHit + Nnewhit];
-    TVector3 **newfXo = new TVector3*[fNHit + Nnewhit];
-    TVector3 **newfXr = new TVector3*[fNHit + Nnewhit];
-    TVector3 **newfMom = new TVector3*[fNHit + Nnewhit];
-
-    for (UInt_t k = 0; k < fNHit; k++){
-	newfGem[k] = fGem[k];
-	newfEdep[k] = fEdep[k];
-	newfTime[k] = fTime[k];
-	newfPID[k]  = fPID[k];
-	newfType[k] = fType[k];
-	newfEntryNumber[k] = fEntryNumber[k];
-
-	newfXi[k]  = fXi[k];
-	newfXo[k]  = fXo[k];
-	newfXr[k]  = fXr[k];
-	newfMom[k] = fMom[k];
-    }	  		
-    for (UInt_t k = 0; k < Nnewhit; k++){
-	newfGem[k+fNHit] = gd->GetHitChamber(k);
-	newfEdep[k+fNHit] = gd->GetHitEnergy(k);
-	newfTime[k+fNHit] = gd->GetHitTime(k);
-	newfPID[k+fNHit]  = gd->GetParticleID(k);
-	newfType[k+fNHit] = gd->GetParticleType(k);
-	newfEntryNumber[k+fNHit] = gd->GetEntryNumber(k);
-
-	// Need to make new pointers so if se destroy the passed
-	// GEMData we don't have dangling pointers
-	newfXi[k+fNHit]  = new TVector3( gd->GetHitEntrance(k) );
-	newfXo[k+fNHit]  = new TVector3( gd->GetHitExit(k) );
-	newfXr[k+fNHit]  = new TVector3( gd->GetHitReadout(k) );
-	newfMom[k+fNHit] = new TVector3( gd->GetMomentum(k) );
-    }	  		
-
-    delete[] fGem;
-    delete[] fEdep;
-    delete[] fTime;
-    delete[] fPID;
-    delete[] fType;
-    delete[] fEntryNumber;
-    delete[] fXi;
-    delete[] fXo;
-    delete[] fXr;
-    delete[] fMom;
-    
-    fNHit += Nnewhit;
-  fGem = newfGem; // chamber with hit
-  fEdep = newfEdep; // energy lost in drift
-  fTime = newfTime; // hit time
-  fPID = newfPID; // particle ID
-  fType = newfType; // particle type
-  fEntryNumber = newfEntryNumber; // entry in file
-  fXi = newfXi;
-  fXo = newfXo;
-  fXr = newfXr;
-  fMom = newfMom;
+  fHitData.insert( fHitData.end(), gd->fHitData.begin(), gd->fHitData.end() );
 
   return;
 }
--- src/TSolGEMData.h
+++ src/TSolGEMData.h
@@ -6,6 +6,7 @@
 
 #include <TRandom.h>
 #include <TVector3.h>
+#include <vector>
 
 class TSolGEMData 
 {
@@ -17,38 +18,38 @@ class TSolGEMData
   void ClearEvent();
   void InitEvent (UInt_t h);
   
-  void SetNHit (UInt_t h) {fNHit = h;};
-  void SetEvent (UInt_t id) {fEvtID = id; };
-  void SetRun (UInt_t r) {fRunID = r;};
+  void SetNHit (UInt_t h)   { fHitData.resize(h); }
+  void SetEvent (UInt_t id) { fEvtID = id; }
+  void SetRun (UInt_t r)    { fRunID = r; }
 
-  void SetMomentum (UInt_t k, const TVector3& p) {*(fMom[k]) = p;};
+  void SetMomentum (UInt_t k, const TVector3& p) { fHitData[k].fMom = p; }
   // Positions are in mm
-  void SetHitEntrance (UInt_t k, const TVector3& xEnt) {*(fXi[k]) = xEnt;};
-  void SetHitExit (UInt_t k, const TVector3& xExit) {*(fXo[k]) = xExit;};
-  void SetHitReadout (UInt_t k, const TVector3& xRead) {*(fXr[k]) = xRead;};
-  void SetVertex (UInt_t k, const TVector3& v) {*(fVert[k]) = v;};
+  void SetHitEntrance (UInt_t k, const TVector3& xEnt) { fHitData[k].fXi = xEnt; }
+  void SetHitExit (UInt_t k, const TVector3& xExit)    { fHitData[k].fXo = xExit; }
+  void SetHitReadout (UInt_t k, const TVector3& xRead) { fHitData[k].fXr = xRead; }
+  void SetVertex (UInt_t k, const TVector3& v)         { fHitData[k].fVert = v; }
   // Energy lost is in eV
-  void SetHitEnergy (UInt_t k, Double_t e) {fEdep[k] = e;};
-  void SetHitTime(UInt_t k, Double_t t) {fTime[k] = t;};
-  void SetHitChamber (UInt_t k, UInt_t c) {fGem[k] = c;};
-  void SetParticleID (UInt_t k, UInt_t pid) {fPID[k] = pid;};
-  void SetParticleType (UInt_t k, UInt_t type) {fType[k] = type;};
-  void SetEntryNumber (UInt_t k, UInt_t n) {fEntryNumber[k] = n;}
+  void SetHitEnergy (UInt_t k, Double_t e)     { fHitData[k].fEdep = e; }
+  void SetHitTime(UInt_t k, Double_t t)        { fHitData[k].fTime = t; }
+  void SetHitChamber (UInt_t k, UInt_t c)      { fHitData[k].fGem  = c; }
+  void SetParticleID (UInt_t k, UInt_t pid)    { fHitData[k].fPID  = pid; }
+  void SetParticleType (UInt_t k, UInt_t type) { fHitData[k].fType = type; }
+  void SetSource (UInt_t k, Int_t n)           { fHitData[k].fSource = n; }
 
-  UInt_t GetNHit() const {return fNHit;};
-  UInt_t GetEvent() const {return fEvtID;};
-  UInt_t GetRun() const {return fRunID;};
-  TVector3& GetMomentum (UInt_t k) const {return *(fMom[k]);};
-  TVector3& GetHitEntrance (UInt_t k) const {return *(fXi[k]);};
-  TVector3& GetHitExit (UInt_t k) const {return *(fXo[k]);};
-  TVector3& GetHitReadout (UInt_t k) const {return *(fXr[k]);};
-  TVector3& GetVertex (UInt_t k) const {return *(fVert[k]);};
-  Double_t GetHitEnergy (UInt_t k) const {return fEdep[k];};
-  Double_t GetHitTime(UInt_t k) const {return fTime[k];};
-  UInt_t GetHitChamber (UInt_t k) const {return fGem[k];};
-  UInt_t GetParticleID (UInt_t k) const {return fPID[k];};
-  UInt_t GetParticleType (UInt_t k) const {return fType[k];};
-  UInt_t GetEntryNumber(UInt_t k) const {return fEntryNumber[k];};
+  UInt_t GetNHit()  const { return fHitData.size(); }
+  UInt_t GetEvent() const { return fEvtID; }
+  UInt_t GetRun()   const { return fRunID; }
+  const TVector3& GetMomentum (UInt_t k)    const { return fHitData[k].fMom; }
+  const TVector3& GetHitEntrance (UInt_t k) const { return fHitData[k].fXi; }
+  const TVector3& GetHitExit (UInt_t k)     const { return fHitData[k].fXo; }
+  const TVector3& GetHitReadout (UInt_t k)  const { return fHitData[k].fXr; }
+  const TVector3& GetVertex (UInt_t k)      const { return fHitData[k].fVert; }
+  Double_t GetHitEnergy (UInt_t k)    const { return fHitData[k].fEdep; }
+  Double_t GetHitTime(UInt_t k)       const { return fHitData[k].fTime; }
+  UInt_t GetHitChamber (UInt_t k)     const { return fHitData[k].fGem; }
+  UInt_t GetParticleID (UInt_t k)     const { return fHitData[k].fPID; }
+  UInt_t GetParticleType (UInt_t k)   const { return fHitData[k].fType; }
+  Int_t  GetSource(UInt_t k)          const { return fHitData[k].fSource; }
   void AddGEMData(TSolGEMData *);
 
   void Print() const;
@@ -56,22 +57,23 @@ class TSolGEMData
 
  private:
 
-  UInt_t fNHit; // number of hits in event
-
   UInt_t fRunID, fEvtID;
 
-  // Hits
+  // Hit data
+  struct GEMHitData_t {
+    UInt_t    fGem;
+    Double_t  fEdep;  // energy lost in drift
+    Double_t  fTime;  // hit time
+    UInt_t    fPID;   // particle ID
+    Int_t     fSource;// file source (0 = signal, >0 backgrounds)
+    UInt_t    fType;  // particle counter: 1 = primary, >1 secondaries
+    TVector3  fXi;    // entrance point in drift
+    TVector3  fXo;    // exit point in drift
+    TVector3  fXr;    // entrance point in readout
+    TVector3  fMom;   // momentum of the particle
+    TVector3  fVert;  // vertex position
+  };
+  std::vector<GEMHitData_t> fHitData;
 
-  UInt_t *fGem; // chamber with hit
-  Double_t *fEdep; // energy lost in drift
-  Double_t *fTime; // hit time
-  UInt_t *fPID; // particle ID
-  UInt_t *fType;  // 0 for signal, >0 for backgrounds
-  TVector3 **fXi; // entrance point in drift
-  TVector3 **fXo; // exit point in drift
-  TVector3 **fXr; // entrance point in readout
-  TVector3 **fMom; // momentum of the particle
-  TVector3 **fVert; // vertex position
-  UInt_t *fEntryNumber;
 };
 #endif
