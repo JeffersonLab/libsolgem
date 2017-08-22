1881b6a49d9e5ffd9dab2c790def3a88796ff14a
Eric Fuchey <efuchey@ifarm1101.jlab.org>
2017-08-22 18:06:30 -0400

    Added file DigitizationPass and ReplayMCDigitized.C in directory example, respectively for digitization and analysis.

---
+++ example/DigitizationPass.C
@@ -0,0 +1,247 @@
+// Example "replay" script
+//#define DEBUG 1
+
+void DigitizationPass(int fspec = 1, 
+		      int Nmax = 1000, 
+		      int nbacktoadd = 2, // number of background *files* to add... 
+		      bool print = false){
+  printf("\n** This gets called with 'analyzer' and not 'root' **\n");
+  printf("** If you're getting missing symbol errors, this is likely the cause **\n\n");
+  
+  gSystem->Load("../libsolgem.so");
+  
+  ////////////////////////////////////////////////////////////////
+  
+  int Ngood = 0;
+  
+  TSBSGEMChamber *ddy;
+  TSBSSpec *dds;
+  TSBSSimGEMDigitization *ddd;
+  TSBSSimDecoder *dde;
+
+  char* outname;
+  char* bg = "bkgd";
+  if(nbacktoadd==0)bg = "nobkgd";
+
+  char* infile_sig;
+  char* infile_bkgd_prefix;
+  
+  TSolDBManager* manager = TSolDBManager::GetInstance();
+  switch(fspec){
+  case(1):
+    manager->LoadGeneralInfo("db_generalinfo_bbgem.dat");
+    manager->LoadGeoInfo("g4sbs_bbgem");
+    dds = new TSBSSpec ("g4sbs_bbgem", "BB spectrometer");
+    outname = Form("digitized_bbgem_%s.root", bg);
+    infile_sig = "/work/halla/sbs/efuchey/gmn_elastic/gmn13.5_elastic_sig_20170504_10/elastic_0.root";
+    infile_bkgd_prefix = "/volatile/halla/sbs/efuchey/gmn13.5_beam_bkgd_20170630_14";
+    dds->Init();
+    break;
+  case(3):
+    manager->LoadGeneralInfo("db_generalinfo_ft.dat");
+    manager->LoadGeoInfo("g4sbs_ft");
+    dds = new TSBSSpec ("g4sbs_ft", "SBS spectrometer FT");
+    outname = Form("digitized_ft_%s.root", bg);
+    infile_sig = "/volatile/halla/sbs/efuchey/gep12_elastic_sig_20170727_14/elastic_0.root";
+    infile_bkgd_prefix = "/work/halla/sbs/efuchey/gep_beam_bkgd/gep12_beam_bkgd_20170114_11";
+    dds->Init();
+    break;
+  case(4):
+    manager->LoadGeneralInfo("db_generalinfo_fpp.dat");
+    manager->LoadGeoInfo("g4sbs_fpp");
+    dds = new TSBSSpec ("g4sbs_fpp", "SBS spectrometer FPP");
+    outname = Form("digitized_fpp_%s.root", bg);
+    infile_sig = "/volatile/halla/sbs/efuchey/gep12_elastic_sig_20170727_14/elastic_0.root";
+    infile_bkgd_prefix = "/work/halla/sbs/efuchey/gep_beam_bkgd/gep12_beam_bkgd_20170114_11";
+    dds->Init();
+    break;
+  default:
+    cout << "No corresponding geometry; choose: " << endl 
+	 << "1 (BBGEM)" << endl << "3 (FT)" << endl << "4 (FPP)" << endl;
+    return;
+    break;
+  }
+  
+  cout << "1  " << outname << " " << &outname << endl;
+  
+  for(int i_ch = 0; i_ch<manager->GetNChamber()*manager->GetNSector(); i_ch++){
+    ddy = new TSBSGEMChamber (Form("gem%d",i_ch),Form("Test chamber %d", i_ch));
+    ddy->SetApparatus(dds);
+    if( ddy->Init() )
+      return;
+    dds->AddGEM (ddy);
+  }
+  printf("\n");
+
+  //cout << outname << " " << &outname << endl;
+  
+  if(print)dds->Print();
+    
+  ddd = new TSBSSimGEMDigitization (*dds,"ratedig");
+  //ddd = new TSBSSimGEMDigitization (*dds,"testdigitization");
+  ddd->SetMapSector(false);
+    
+  ////////////////////////////////////////////////////////////////
+  
+  TSBSGeant4File *f = new TSBSGeant4File(infile_sig);
+  printf("The filename returned is %s\n", f->GetFileName());
+  f->SetSource(0);
+
+  int res;
+    
+  res = f->Open();
+
+  if( res != 1 ){
+    printf("Opening g4sbs file returned %s\n", res);
+    return;
+  }
+
+  ////////////////////////////////////////////////////////////////
+    
+  int nevent = 0;
+    
+  int  ndata, i;
+  TSolGEMData *gd, *gb;
+  g4sbsgendata *gen;
+  
+  cout << "creating file " << outname << endl;
+  ddd->InitTree (*dds, outname);
+    
+  printf("Digitizing events\n");
+  ndata = 0;
+    
+  Nmax = TMath::Min((Long64_t)Nmax, f->GetEntries());
+    
+  int hadback = 1;
+  
+  int N_bg_file_g = 0;
+  
+  int d_flag_readevent = 0;
+  while( f->ReadNextEvent(d_flag_readevent) && hadback && nevent<Nmax ){
+
+    if(nevent%100==0){
+      cout << "Evt " << nevent << endl;
+    }
+    
+    if(f->GetNData()==0){
+      if(print)cout << "No hits, skip evt " << nevent << endl;
+      nevent++;
+      continue;
+    }
+    
+    gd = f->GetGEMData();
+    if(f->GetNGen()>0){
+      gen = f->GetGenData(0);
+      Ngood++;
+    }else{
+      cout << "No generated data for event " << nevent 
+	   << ", skip it (Nhits = " << f->GetNData() << ")" << endl;
+      nevent++;
+      continue;
+    }
+      
+    ddd->SetTreeEvent((*gd), (*f), nevent);
+    
+    /*
+    if(print){// || nevent==40
+      cout << "number of hits in GEM data " << gd->GetNHit() << endl;
+      while(ndata<gd->GetNHit()){
+	  
+	//if(gd->GetParticleID(ndata)>1)continue;
+	gd->Print();
+	cout << "hit number " << ndata << endl;
+	gd->PrintHit(ndata);
+	ndata++;
+      }
+    }
+    */
+    ddd->Digitize(*gd, *dds);
+      
+    // Access to generated vertex and momentum
+    // gen->GetV();
+    // gen->GetP();
+    // gen->GetWeight();
+      
+    // Add some number of background files...
+    int N_bg_file_g_post = N_bg_file_g+nbacktoadd;
+ 
+    if(nbacktoadd){
+      for(int Nfile = N_bg_file_g; Nfile < N_bg_file_g_post; Nfile++){
+	//if(print)cout << N_bg_file_g << " <= " << Nfile << " < " << N_bg_file_g+nbacktoadd << endl;
+	TSBSGeant4File *fback = new TSBSGeant4File(Form("%s/beam_bkgd_%d.root",infile_bkgd_prefix, Nfile));
+	int open = fback->Open();
+	if(!open){
+	  N_bg_file_g_post++;
+	  N_bg_file_g++;
+	  
+	  if(N_bg_file_g>=2000){
+	    int n_temp = Nfile;
+	    Nfile = N_bg_file_g_post-n_temp;
+	    N_bg_file_g_post = nbacktoadd;
+	    N_bg_file_g = 0;
+	  }
+	  //if(print)cout << Form("/group/exjpsi/eric/31722/beam_bkgd_%d.root does not exist", Nfile) << endl;
+	  continue;
+	}
+	  
+	fback->SetSource(1);
+	  
+	int backidx = 0;
+	//while( hadback = fback->ReadNextEvent() && backidx < nbacktoadd ){
+	while( backidx < fback->GetEntries() ){
+	  hadback = fback->ReadNextEvent();
+	  gb = fback->GetGEMData();
+	    
+	  if(print && gb->GetNHit()>0){
+	    cout << "Bkgd evt: " << gb->GetEvent() << ", number of hits " 
+		 << gb->GetNHit() << endl;
+	    int nback = 0;
+	    while(nback<gb->GetNHit()){
+	      //if(gd->GetParticleID(ndata)>1)continue;
+	      gb->Print();
+	      cout << "hit number " << nback << endl;
+	      gb->PrintHit(nback);
+	      nback++;
+	    }
+	    
+	  }
+	  
+	  ddd->AdditiveDigitize(*gb, *dds);
+	    
+	  // //Randomize times based on gate width
+	  // for( int bidx = 0; bidx < gb->GetNHit(); bidx++ ){
+	  //   double timeshift = gRandom->Uniform(-ddd->GetGateWidth(), 75.0 );//ns
+	  //   gb->SetHitTime(bidx, gb->GetHitTime(bidx) + timeshift );
+	  // }	
+	  // //gd->AddGEMData(gb);
+	  backidx++;
+	}
+	  
+	// if( backidx != nbacktoadd ){
+	// printf("Warning:  Not enough background events to be added (%d)\n", backidx);
+	// }
+	  
+	fback->Close();
+      }
+      //if(print)cout << "new number of hits in GEM data " << gd->GetNHit() << endl;
+      N_bg_file_g = N_bg_file_g_post;
+    }//end if nbacktoadd
+    
+    if(N_bg_file_g>=2000)N_bg_file_g = 0;
+    
+    ddd->FillTree();
+      
+    //if(nevent==7)ddd->GetEvent()->Print("all");
+    if(print)ddd->GetEvent()->Print("all");
+    //ddd->GetEvent()->Print("clust");
+        
+    delete gd;
+    nevent++;
+  }
+  printf("Completed %d events total: %d good events \n", nevent, Ngood);
+
+  ddd->WriteTree();
+  ddd->CloseTree();
+  
+  cout << "Tree closed" << endl;
+}
---
+++ example/ReplayMCDigitized.C
@@ -0,0 +1,166 @@
+#ifndef __CINT__
+
+#include "SBSSpec.h"
+#include "TSBSSimDecoder.h"
+#include "TSolSimFile.h"
+
+#include "THaInterface.h"
+#include "THaGlobals.h"
+#include "THaTextvars.h"
+#include "THaAnalyzer.h"
+#include "THaDetector.h"
+
+#include "TSystem.h"
+#include "TList.h"
+#include "TString.h"
+#include "TFile.h"
+#include "TVector3.h"
+
+#include <iostream>
+
+#endif
+
+void ReplayMCDigitized(const char* filename = "digitized", 
+		       const char* detsuffix = "bbgem",
+		       bool bkgd,
+		       Int_t nevent = -1, 
+		       Int_t nseg = 0, 
+		       bool do_cuts = true )
+{
+  if( nseg < 0 || nseg > 100 ) {
+    cerr << "Invalid number of run segments = " << nseg
+	 << ", must be 0-100" << endl;
+    return;
+  }
+  bool do_parts = true;
+  if( nseg == 0 ) {
+    do_parts = false;
+    nseg = 1;
+  }
+  
+  char* bg = "nobkgd";
+  if(bkgd)bg = "bkgd";
+  
+  gSystem->Load("libsolgem.so");
+  gSystem->Load("libTreeSearch-SBS.so");
+  
+  TSolDBManager* manager = TSolDBManager::GetInstance();
+  manager->LoadGeneralInfo(Form("db_generalinfo_%s.dat", detsuffix));
+  manager->LoadGeoInfo(Form("g4sbs_%s", detsuffix));
+
+  int Ns = manager->GetNSector();
+  const int Nsect = Ns;
+  
+  cout << "detector " << detsuffix << ": " << Nsect << " sectors." << endl;
+  
+  // dde = new TSBSSimDecoder();
+  // dde->SetCrateMapName("db_sbssim_cratemap.dat");
+  
+  THaInterface::SetDecoder( TSBSSimDecoder::Class() );
+  
+  cout << "Reading " << detsuffix << endl;
+  THaApparatus* SBS_GEMdet = new SBS::SBSSpec( Form("sbs_%s",detsuffix), "SBS / BB GEMs", Nsect );
+  gHaApps->Add( SBS_GEMdet );
+  cout << "Just read " << detsuffix << endl;
+
+  SBS_GEMdet->Print("DET");
+
+  TString db_prefix = SBS_GEMdet->GetName();
+  db_prefix += ".tracker";
+  gHaTextvars->Add( "DET", db_prefix.Data() );
+  gHaTextvars->Add( "APP", SBS_GEMdet->GetName() );
+  
+  THaAnalyzer* analyzer = new THaAnalyzer;
+  
+  TString rootfile(Form("%s_%s_%s", filename, detsuffix, bg)), infile0(Form("%s_%s_%s", filename, detsuffix, bg));
+  TString odeffile("sbssim.odef"), cutfile(Form("sbs_%ssim.cuts",detsuffix));
+  rootfile.Append("_replayed_new.root");
+  analyzer->EnableBenchmarks();
+  analyzer->SetOutFile(rootfile);
+  analyzer->SetOdefFile(odeffile);
+  if( do_cuts ) analyzer->SetCutFile(cutfile);
+  analyzer->SetSummaryFile(Form("%s_%s_new.sum", filename, detsuffix));
+  analyzer->SetCrateMapFileName("sbssim_cratemap");
+  
+  //static int Nrun = TMath::Max(nseg,1);
+  THaRunBase* run[0];
+  TString title0 = "Digitized MC data";
+  for( int i=0; i<nseg; ++i ) {
+    TString title(title0), infile(infile0);
+    if( do_parts ) {
+      title.Append(Form(" part %d", i+1));
+      infile.Append(Form("_p%d", i+1));
+    }
+    infile.Append(".root");
+    run[i] = new TSolSimFile(infile,title);
+  }
+  if( nseg == 1 && nevent > 0 )
+    run[0]->SetLastEvent(nevent);
+
+  bool fail = true;
+  if( analyzer->Init(run[0]) == 0 ) {
+    cout << "initialization successful..." << endl;
+    THaDetector* tracker[Nsect];
+    for(int ns = 0; ns < Nsect; ns++){
+      //THaDetector* 
+      cout << ns << " " << tracker[ns] << endl;
+      tracker[ns] = SBS_GEMdet->GetDetector(Form("tracker.%d", ns+1));
+      tracker[ns]->Print("");
+      if( tracker[ns] ) {
+	// The SBS trackers' origin really is the origin of the first plane
+	Double_t z0 = tracker[ns]->GetOrigin().Z();
+	cout << "z0 = " << z0 << endl;
+	manager->SetZ0(z0);
+      } else {
+	cerr << "ERROR: cannot get tracker detector! z0 may be wrong" << endl;
+      }
+      // Emulate dummy calorimeter planes
+      // TSBSSimDecoder::SetCaloZ(3.20);  // Calo front is at z = 320 cm
+      // TSBSSimDecoder::SetCaloRes(0.01);  // Calo resolution 1 cm (sigma)
+      // TSBSSimDecoder::EmulateCalorimeter(false);
+    }//end loop on trackers
+    /*
+    THaDetector* tracker = SBS_GEMdet->GetDetector("tracker.1");
+    tracker->Print("");
+    if( tracker ) {
+      // The SBS trackers' origin really is the origin of the first plane
+      Double_t z0 = tracker->GetOrigin().Z();
+      cout << "z0 = " << z0 << endl;
+      manager->SetZ0(z0);
+    } else {
+      cerr << "ERROR: cannot get tracker detector! z0 may be wrong" << endl;
+    }
+    */
+    manager->EmulateCalorimeter(false);
+    
+    // Process the runs
+    Int_t ret = 0, ntotal = 0;
+    for( int i=0; i<nseg && ret >= 0; ++i ) {
+      //cout << "processing segment i / nseg : " << i << "/" << nseg << endl;
+      if( i>0 )
+	run[i]->SetDate(run[0]->GetDate());
+      //cout << "Start processing " << endl;
+      ret = analyzer->Process(run[i]);
+      cout << "ret = " << ret << endl;
+      if( ret > 0 )
+	ntotal += ret;
+    }
+    fail = (ret < 0 );
+    if( fail )
+      cerr << "Terminated with analyzer error = " << ret << endl;
+    else
+      cout << "Analyzed " << ntotal << " events" << endl;
+    analyzer->Close();
+  }
+  
+  for( int i=0; i<nseg; ++i ) {
+    delete run[i]; run[i] = 0;
+  }
+  delete analyzer; analyzer = 0;
+  gHaApps->Delete();
+  //}
+  
+  //TFile* f =
+  if( !fail )
+    new TFile(rootfile,"r");
+} 
